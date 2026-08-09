20d000dc47a1f5195340de4a92e520375e9f3adc
sohrab09 <sohrab.cse9@gmail.com>
2026-08-09 20:00:12 +0600

    Change local base url to server url.

--- src/redux/app/https.js
+++ src/redux/app/https.js
@@ -1,14 +1,16 @@
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const baseQuery = fetchBaseQuery({
     baseUrl: API_BASE_URL,
     prepareHeaders: (headers) => {
         const token = localStorage.getItem("redpulse_token");
+
         if (token) {
             headers.set("authorization", `Bearer ${token}`);
         }
+
         return headers;
     },
 });
